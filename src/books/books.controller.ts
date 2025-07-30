workshops-de/workshop-nestjs

solution--fundamentals-pipe-validate-property

src/books/books/books.controller.ts
@@ -1,5 +1,16 @@
-import { Body, Controller, Get, NotFoundException, Param, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+  UsePipes
+} from '@nestjs/common';
+import { RequestValidationPipe } from '../../request-validation/request-validation.pipe';
 import { Book } from '../book/book';
+import { CreateBookDto } from '../dtos/create-book.dto/create-book.dto';
 import { BooksService } from './books.service';
 
 @Controller('books')
@@ -9,6 +20,14 @@ export class BooksController {
   @Get()
   getAll(): Promise<Book[]> {
     return this.booksService.getAll();
+  }
+
+  @Post()
+  @UsePipes(new RequestValidationPipe())
+  async create(@Body() dto: CreateBookDto): Promise<Pick<Book, 'id'>> {
+    const createdBook = await this.booksService.create(dto);
+
+    return { id: createdBook.id };
   }
 
   @Put(':id/order')

src/books/books/books.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { Book } from '../book/book';
 import { randomUUID } from 'crypto';
 import { DomainException } from '../domain.exception';
+import { CreateBookDto } from '../dtos/create-book.dto/create-book.dto';
 
 @Injectable()
 export class BooksService {
@@ -25,6 +26,14 @@ export class BooksService {
     const candidate = this.bookCollection.find(book => book.id === id) ?? null;
 
     return Promise.resolve(candidate);
+  }
+
+  async create(dto: CreateBookDto): Promise<Book> {
+    const book = new Book({ id: randomUUID(), ...dto, rating: 0, amount: dto.amount ?? 0 });
+
+    this.bookCollection.push(book);
+
+    return Promise.resolve(book);
   }
 
   order(bookToOrder: Book, dto: { amountOrdered: number }) {

workshops-de/nestjs-workshop

src/books/books.controller.ts
@@ -1,6 +1,17 @@
-import { Body, Controller, Get, NotFoundException, Param, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+  UsePipes
+} from '@nestjs/common';
+import { RequestValidationPipe } from '../request-validation/request-validation.pipe';
 import { BookEntity } from './book/book.entity';
 import { BooksService } from './books.service';
+import { CreateBookDto } from './dtos/create-book.dto/create-book.dto';
 
 @Controller('books')
 export class BooksController {
@@ -9,6 +20,14 @@ export class BooksController {
   @Get()
   getAll(): Promise<BookEntity[]> {
     return this.booksService.getAll();
+  }
+
+  @Post()
+  @UsePipes(new RequestValidationPipe())
+  async create(@Body() dto: CreateBookDto): Promise<Pick<BookEntity, 'id'>> {
+    const createdBook = await this.booksService.create(dto);
+
+    return { id: createdBook.id };
   }
 
   @Put(':id/order')

src/books/books.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { randomUUID } from 'crypto';
 import { BookEntity } from './book/book.entity';
 import { DomainException } from './domain.exception';
+import { CreateBookDto } from './dtos/create-book.dto/create-book.dto';
 
 @Injectable()
 export class BooksService {
@@ -25,6 +26,14 @@ export class BooksService {
     const candidate = this.bookCollection.find(book => book.id === id) ?? null;
 
     return Promise.resolve(candidate);
+  }
+
+  async create(dto: CreateBookDto): Promise<BookEntity> {
+    const book = new BookEntity({ id: randomUUID(), ...dto, rating: 0, amount: dto.amount ?? 0 });
+
+    this.bookCollection.push(book);
+
+    return Promise.resolve(book);
   }
 
   order(bookToOrder: BookEntity, dto: { amountOrdered: number }) {